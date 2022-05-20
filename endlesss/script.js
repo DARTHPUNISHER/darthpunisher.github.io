b391f13676f8c8b02424cc7c899fdbdf77b12824
DARTHPUNISHER <94704391+DARTHPUNISHER@users.noreply.github.com>
2022-05-20 23:38:16 +0530

    Update script.js

--- endlesss/script.js
+++ endlesss/script.js
@@ -1,3 +1,13 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZWJPS1SQB4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZWJPS1SQB4');
+</script>
+
 let searchBtn = document.querySelector('#search-btn');
 let searchForm = document.querySelector('.header .search-form');
 
